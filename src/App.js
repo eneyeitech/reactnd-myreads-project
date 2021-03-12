7a1acc77329a13dc4c39b57cbc927feddb55fed1
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2021-03-12 20:38:58 +0000

    Routing implemented

--- src/App.js
+++ src/App.js
@@ -103,16 +103,19 @@ const currentlyReadingBooks = books.filter((book)=>(
     
     return (
       <div className="app">
-        {this.state.showSearchPage ? (
+      <Route path='/search' render={({history})=>(
           <div className="search-books">
             <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
+             
+<Link className="close-search"
+                    to='/'>
+                    Close
+                </Link>
               <div className="search-books-input-wrapper">
                 {/*
                   NOTES: The search from BooksAPI is limited to a particular set of search terms.
                   You can find these search terms here:
                   https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
                   However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
                   you don't find a specific author or title. Every search is limited by search terms.
                 */}
@@ -127,7 +130,8 @@ const currentlyReadingBooks = books.filter((book)=>(
               <ListBooks showingBooks={searchedBooks} onSelectHandler={this.selectHandler}/>
             </div>
           </div>
-        ) : (
+)} />
+        <Route exact path='/' render={() => (
           <div className="list-books">
             <div className="list-books-title">
               <h1>MyReads</h1>
@@ -155,10 +159,13 @@ const currentlyReadingBooks = books.filter((book)=>(
               </div>
             </div>
             <div className="open-search">
-              <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+      				<Link
+                    to='/search'>
+                    Add a book
+                </Link>
             </div>
           </div>
-        )}
+        )}/>
       </div>
     )
   }
